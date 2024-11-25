a5d34d7a276b6ab8b458623c929bfed2de1b7fd3
EmanuelCJ <emanuelcastrillo95@gmail.com>
2024-11-25 00:56:14 -0300

    EmanuelCJ-Agregando-Proyecto-25-11-2024

---
+++ my_porfolio/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar></NavBar>
+    <HeroSection></HeroSection>
+  </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import HeroSection from '@/components/HeroSection.vue'
+</script>
+<style scoped></style>
